a2afaa5145c742e27f6b9400e79c382da1a8e3fb
Diego <consultoresartificialbrain@gmail.com>
2021-01-27 16:32:13 -0600

    subiendo a la parte web

---
+++ build/precache-manifest.e9692fee38ee46410ed7be04da241b9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9bca4e16bdb746c03a4ac618cc410e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98edba08ed50d3801693",
+    "url": "/static/css/2.13cf92e7.chunk.css"
+  },
+  {
+    "revision": "75bc5defa717a8d2469a",
+    "url": "/static/css/main.fad061e4.chunk.css"
+  },
+  {
+    "revision": "98edba08ed50d3801693",
+    "url": "/static/js/2.57e4aa54.chunk.js"
+  },
+  {
+    "revision": "81f4270d70d06875704b839639c27d8e",
+    "url": "/static/js/2.57e4aa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bc5defa717a8d2469a",
+    "url": "/static/js/main.72174029.chunk.js"
+  },
+  {
+    "revision": "e621ac90fd4b80177548",
+    "url": "/static/js/runtime-main.5aecc905.js"
+  },
+  {
+    "revision": "3c6258e568480fc1e8bd769e42d7579c",
+    "url": "/static/media/bg-tarjeta-01.3c6258e5.jpg"
+  },
+  {
+    "revision": "6611645bd370943ebc7139d4062b9377",
+    "url": "/static/media/chip-tarjeta.6611645b.png"
+  },
+  {
+    "revision": "ae0915fe18dcc1bafdd686a15e448ed6",
+    "url": "/static/media/mastercard.ae0915fe.png"
+  },
+  {
+    "revision": "d2eb00b710fa2ee2c6a4408cb42f6fb4",
+    "url": "/static/media/visa.d2eb00b7.png"
+  }
+]);
